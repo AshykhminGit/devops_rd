ca01bf77d131dfe1fed282ac6fc9132dd50f52ea
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-06-24 15:08:41 +0300

    Update find_by_size.sh

--- scripts/find_by_size.sh
+++ scripts/find_by_size.sh
@@ -3,4 +3,6 @@
 
 #find "$PWD" -type f -name '*.txt' -printf "%k %h/%f\n" | sort -rg
 
+#создать скрипт,  который бы выводил список файлов в папке /var/log, сортировал бы по размеру и выводил в текстовый файл
+
 find /var/log -type f -name '*.*' -printf "%k %h/%f\n" | sort -rg >> logs_sizes.txt
