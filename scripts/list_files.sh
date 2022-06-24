cc63d1bb62ecbbb1967418ce2f673a40e0aff944
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-06-24 15:18:19 +0300

    Update list_files.sh

--- scripts/list_files.sh
+++ scripts/list_files.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#поиск директорий и файлов и вывод списка в текстовые файлы
 for file in /usr/*
 do
 if [ -d "$file" ]
 then
-echo "$file is a directory"
+echo "$file is a directory" >> direcories.txt
 elif [ -f "$file"]
 then 
-echo "$file is a file"
+echo "$file is a file". >> files.txt
 fi
 done
 
