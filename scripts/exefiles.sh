c5f9cdb1853be8417c175baa24b338390ba6ec2a
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-06-24 15:15:10 +0300

    Update exefiles.sh

--- scripts/exefiles.sh
+++ scripts/exefiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#создать скрипт,  который проверял бы файлы в папке /usr находил из них файлы с атрибутом на исполнение и выводил в файл executable.txt
 for directory in $(echo $PATH | tr ':' '\n'); do
     find $directory -type f -perm /111 -ls  >> executable.txt
 done
