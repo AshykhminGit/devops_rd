6d8fd8ab8bba6cee3f9c154741ce6d8507cbdac7
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-07-26 20:07:04 +0200

    Update default_route.sh

--- Lesson10/default_route.sh
+++ Lesson10/default_route.sh
@@ -1,4 +1,4 @@
 #Напишите команду для задания default route для вашей машины с использованием сетевого интерфейса eth0.
 
 sudo ip route add default via 192.168.1.254 dev eth0
-sudo route add default gw 192.168.1.254 dev eth0
+sudo route add default gw 192.168.1.1 dev eth0
