277ead67d3bb13f724e6bbf1529bbded4bcb1b7d
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-08-04 19:45:08 +0200

    Update status4.sh

--- status4.sh
+++ status4.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+#создаем пременную для поиска апача. нэйм для имени PID для номера процесса
 name='apache2'
-PID=`ps aux| grep [/]usr/sbin/apache2 | awk '{print $2}'`
+PID=`ps aux| grep [/]usr/sbin/apache2 | awk '{print $2}'`.  # ``    такие кавычки нужны для выполнения команды
 
 # DEBUG info
 echo $PID
-
+#делаем цикл в которром ищем пиды если пид есть отпраляем ему стоп
 for PIDcycle in $PID; do
     sudo kill  -STOP $PIDcycle
 done
-
+#ждем 10 секунд
 sleep 10
-
+# второй цикл на поиск отсавшихся в живых Если находит то  уже киляет
 for PIDcycle in $PID; do
     if ps -p $PIDcycle > /dev/null; then
         echo "$PIDcycle is running"
